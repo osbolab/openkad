21273017d03f60e5338581ef5c05b12b5d39823d
yoav.kantor <yoav.kantor@6f7e9320-9a2e-3c67-76ab-11ce00100491>
2012-08-15 14:33:39 +0000

    git-svn-id: http://openkad.googlecode.com/svn/trunk@54 6f7e9320-9a2e-3c67-76ab-11ce00100491

---
+++ openkad/src/il/technion/ewolf/kbr/openkad/msg/ShadesFindNodeRequest.java
@@ -0,0 +1,56 @@
+package il.technion.ewolf.kbr.openkad.msg;
+
+import il.technion.ewolf.kbr.Key;
+import il.technion.ewolf.kbr.Node;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+
+/**
+ * A findNode request as defined in the kademlia protocol
+ * 
+ * @author eyal.kibbar@gmail.com
+ *
+ */
+public class ShadesFindNodeRequest extends KadRequest {
+
+	private static final long serialVersionUID = -2437918697668927600L;
+	private Key key;
+	private boolean searchCache;
+	
+	@Inject
+	ShadesFindNodeRequest(
+			@Named("openkad.rnd.id") long id,
+			@Named("openkad.local.node") Node src) {
+		super(id, src);
+	}
+	/**
+	 * 
+	 * @return the key we are searching
+	 */
+	public Key getKey() {
+		return key;
+	}
+	
+	public ShadesFindNodeRequest setKey(Key key) {
+		this.key = key;
+		return this;
+	}
+
+	@Override
+	public ShadesFindNodeResponse generateResponse(@Named("openkad.local.node") Node localNode) {
+		return new ShadesFindNodeResponse(getId(), localNode);
+	}
+
+	
+	public ShadesFindNodeRequest setSearchCache(boolean searchCache) {
+		this.searchCache = searchCache;
+		return this;
+	}
+	
+	public boolean shouldSearchCache() {
+		return searchCache;
+	}
+	
+
+}
